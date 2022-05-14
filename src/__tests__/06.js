8994e597fc397327063ad954bbfc9a1db88b0945
OtacilioN <contato@otaciliomaia.com>
2022-05-13 23:59:58 -0300

    Add exercise version to tests

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/06'
-// import App from '../exercise/06'
+// import App from '../final/06'
+import App from '../exercise/06'
 
 beforeAll(() => {
   jest.spyOn(global, 'alert').mockImplementation(() => {})
--- src/__tests__/07.js
+++ src/__tests__/07.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import {render, screen, within} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/07'
-// import App from '../exercise/07'
+// import App from '../final/07'
+import App from '../exercise/07'
 
 test('renders', () => {
   const {container} = render(<App />)
